fa8254d2a13324fe13c7597c25c105dcd30fc8d2
Noof M <noofm@Noofs-MacBook-Air.local>
2026-01-30 19:56:00 +0300

    Implement my_name_is function

--- today.py
+++ today.py
@@ -1,7 +1,10 @@
-# pylint: disable=C0116,C0303
-
 # $CHALLENGIFY_BEGIN
 def my_name_is():
     return "maha126017-png"
+# $CHALLENGIFY_END
+
+# $CHALLENGIFY_BEGIN
+def my_age_is():
+    return 21
 # $CHALLENGIFY_END
 
